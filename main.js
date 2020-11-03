a21a7758313e0668bfabc15ac74aa86ecbdc620a
Steve Min <stevekmin@gmail.com>
2020-11-03 07:38:50 -0800

    init time variables

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const container = document.querySelector('.container');
 const text = document.querySelector('#text');
 
+const totaltime = 7500;
+const breathTime = (totaltime / 5) * 2;
+const holdTime = totalTime / 5;
+
 function breathAnimation() {}
