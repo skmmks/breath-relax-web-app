dab650e05c08f83ee71fb1c8d626cdb45dc24659
Steve Min <stevekmin@gmail.com>
2020-11-03 07:43:30 -0800

    update breathAnimation function add initial breath text change class add a setTimeout to hold breath

--- main.js
+++ main.js
@@ -5,4 +5,11 @@ const totaltime = 7500;
 const breathTime = (totaltime / 5) * 2;
 const holdTime = totalTime / 5;
 
-function breathAnimation() {}
+function breathAnimation() {
+  text.innerHTML = 'Breath In...';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold...';
+  }, breathTime);
+}
