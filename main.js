6a74ae1a1d64d1843c230e64f16e6cef100a0610
Steve Min <stevekmin@gmail.com>
2020-11-04 07:09:40 -0800

    change and fix totalTime variable

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('.container');
 const text = document.querySelector('#text');
 
-const totaltime = 7500;
-const breathTime = (totaltime / 5) * 2;
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
 breathAnimation();
